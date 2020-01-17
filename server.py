0a0d36b83146df491ec515d17a3958bdd330303a
Calix <wingtat.calix@gmail.com>
2020-01-17 11:24:00 +0800

    modified text

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/") # Revisit decorators if you unclear of this syntax
 def index():
-    return '<h1>Why so easy2</h1>'
+    return '<h1>Why so easy!</h1>'
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run()
